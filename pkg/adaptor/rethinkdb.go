59daaf517a713ecf44e6625640065d7b4f23e661
ShockiTV <ShockiTV@users.noreply.github.com>
2015-08-27 18:16:38 +0200

    fix

--- pkg/adaptor/rethinkdb.go
+++ pkg/adaptor/rethinkdb.go
@@ -9,8 +9,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/compose/transporter/pkg/message"
-	"github.com/compose/transporter/pkg/pipe"
+	"github.com/ShockiTV/transporter/pkg/message"
+	"github.com/ShockiTV/transporter/pkg/pipe"
 	version "github.com/hashicorp/go-version"
 	gorethink "gopkg.in/dancannon/gorethink.v1"
 )
